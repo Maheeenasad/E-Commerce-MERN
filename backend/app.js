96c1e305ea36025aa833a4bc235a701dc1acd0dd
Maheen Asad <maheenasad19@gmail.com>
2024-07-09 18:51:25 +0500

    new file:   backend/app.js 	new file:   backend/config/.env 	new file:   backend/db/Database.js	new file:   backend/middleware/auth.js 	new file:   backend/middleware/catchAsyncErrors.js 	new file:   backend/middleware/error.js 	new file:   backend/model/user.js 	new file:   backend/server.js 	new file:   backend/utils/ErrorHandler.js 	new file:   node_modules/.bin/mime 	new file:   node_modules/.bin/mime.cmd 	new file:   node_modules/.bin/mime.ps1

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const ErrorHandler = require("./utils/ErrorHandler");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/test", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({
+      path: "backend/config/.env"
+    });
+  }
+
+
+// it's for ErrorHandling
+app.use(ErrorHandler);
+
+  module.exports = app;
+
---
+++ backend/middleware/auth.js
@@ -0,0 +1,5 @@
+const ErrorHandler = require("../utils/ErrorHandler");
+const catchAsyncErrors = require("./catchAsyncErrors");
+const jwt = require("jsonwebtoken");
+const User = require("../model/user");
+const Shop = require("../model/shop");
